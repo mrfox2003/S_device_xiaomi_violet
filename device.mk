4ec4ef75eac81c57321b411f1c033d385cfa462f
Nipin NA <nipinna0@gmail.com>
2021-05-10 09:59:41 +0530

    Add GoogleCameraGo,Gallery,RetroMusic

--- device.mk
+++ device.mk
@@ -149,7 +149,6 @@ PRODUCT_PACKAGES += \
     libdng_sdk.vendor \
     libgui_vendor \
     libxml2 \
-    Camera2 \
     vendor.qti.hardware.camera.device@1.0.vendor
 
 PRODUCT_COPY_FILES += \
@@ -334,6 +333,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     NoCutoutOverlay \
     NotchBarKiller
+
+# Packages
+PRODUCT_PACKAGES += \
+    SimpleGalleryPro \
+    GoogleCameraGo \
+    RetroMusic
 
 # Perf
 PRODUCT_PACKAGES += \
