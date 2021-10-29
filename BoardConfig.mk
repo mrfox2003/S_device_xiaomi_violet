3f86cdbaedf59bb696f82edc5e3406d22685313f
Niranjan BR <niranjankannan2003@gmail.com>
2021-10-29 13:42:43 +0000

    violet: Include Sepolicy
    Signed-off-by: Niranjan BR <niranjankannan2003@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -208,11 +208,11 @@ VENDOR_SECURITY_PATCH := 2021-05-01
 
 # Sepolicy
 SELINUX_IGNORE_NEVERALLOWS := true
-#TARGET_SEPOLICY_DIR := msmsteppe
-#include device/qcom/sepolicy_vndr/SEPolicy.mk
-#BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
-#BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/public
-#BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+TARGET_SEPOLICY_DIR := msmsteppe
+include device/qcom/sepolicy_vndr/SEPolicy.mk
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/public
+BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # WiFi
 BOARD_HOSTAPD_DRIVER := NL80211
