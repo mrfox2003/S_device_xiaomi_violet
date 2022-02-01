ArianK16a/android_device_xiaomi_davinci

davinci: fod: Add surfaceflinger extension lib
Change-Id: I51fb56c0a20e3cac83afb46ed6a6e2f967159d18

fod/FodExtension.cpp
@@ -1,0 +1,30 @@
+/*
+ * Copyright (C) 2020 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <compositionengine/FodExtension.h>
+
+uint32_t getFodZOrder(uint32_t z, bool touched) {
+    if (touched) {
+        z |= 0x20000000u;
+    }
+
+    return z;
+}
+
+uint32_t getFodUsageBits(uint32_t usageBits, bool touched) {
+    (void) touched;
+    return usageBits;
+}

mrfox2003/S_device_xiaomi_violet

fod/FodExtension.cpp
@@ -1,0 +1,30 @@
+/*
+ * Copyright (C) 2020 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <compositionengine/FodExtension.h>
+
+uint32_t getFodZOrder(uint32_t z, bool touched) {
+    if (touched) {
+        z |= 0x20000000u;
+    }
+
+    return z;
+}
+
+uint64_t getFodUsageBits(uint64_t usageBits, bool touched) {
+    (void) touched;
+    return usageBits;
+}