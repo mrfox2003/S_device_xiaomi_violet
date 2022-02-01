vikas771/android_device_xiaomi_davinci

davinci: power: Adapt extension to new aidl power HAL
Change-Id: I58ac49957d8d28ad904912986d89ac8db4820ff9

power/power-mode.cpp
@@ -14,17 +14,12 @@
  * limitations under the License.
  */
 
-#include <dirent.h>
-#include <fcntl.h>
-#include <string.h>
-#include <unistd.h>
-
-#include "power-feature.h"
-
-#include <hardware/power.h>
+#include <aidl/android/hardware/power/BnPower.h>
+#include <android-base/file.h>
+#include <android-base/logging.h>
 #include <linux/input.h>
-#include <log/log.h>
 
+namespace {
 int open_ts_input() {
     int fd = -1;
     DIR *dir = opendir("/dev/input");
@@ -56,21 +51,53 @@ int open_ts_input() {
 
     return fd;
 }
+}  // anonymous namespace
 
-void set_device_specific_feature(feature_t feature, int state) {
-    switch (feature) {
-        case POWER_FEATURE_DOUBLE_TAP_TO_WAKE: {
+namespace aidl {
+namespace android {
+namespace hardware {
+namespace power {
+namespace impl {
+
+static constexpr int kInputEventWakeupModeOff = 4;
+static constexpr int kInputEventWakeupModeOn = 5;
+
+using ::aidl::android::hardware::power::Mode;
+
+bool isDeviceSpecificModeSupported(Mode type, bool* _aidl_return) {
+    switch (type) {
+        case Mode::DOUBLE_TAP_TO_WAKE:
+            *_aidl_return = true;
+            return true;
+        default:
+            return false;
+    }
+}
+
+bool setDeviceSpecificMode(Mode type, bool enabled) {
+    switch (type) {
+        case Mode::DOUBLE_TAP_TO_WAKE: {
             int fd = open_ts_input();
             if (fd == -1) {
-                ALOGW("DT2W won't work because no supported touchscreen input devices were found");
-                return;
+                LOG(WARNING)
+                    << "DT2W won't work because no supported touchscreen input devices were found";
+                return false;
             }
             struct input_event ev;
             ev.type = EV_SYN;
             ev.code = SYN_CONFIG;
-            ev.value = state ? INPUT_EVENT_WAKUP_MODE_ON : INPUT_EVENT_WAKUP_MODE_OFF;
+            ev.value = enabled ? kInputEventWakeupModeOff : kInputEventWakeupModeOn;
             write(fd, &ev, sizeof(ev));
             close(fd);
+            return true;
         }
+        default:
+            return false;
     }
 }
+
+}  // namespace impl
+}  // namespace power
+}  // namespace hardware
+}  // namespace android
+}  // namespace aidl

mrfox2003/S_device_xiaomi_violet

power/power-mode.cpp
@@ -14,17 +14,12 @@
  * limitations under the License.
  */
 
-#include <dirent.h>
-#include <fcntl.h>
-#include <string.h>
-#include <unistd.h>
-
-#include "power-feature.h"
-
-#include <hardware/power.h>
+#include <aidl/android/hardware/power/BnPower.h>
+#include <android-base/file.h>
+#include <android-base/logging.h>
 #include <linux/input.h>
-#include <log/log.h>
 
+namespace {
 int open_ts_input() {
     int fd = -1;
     DIR *dir = opendir("/dev/input");
@@ -56,21 +51,53 @@ int open_ts_input() {
 
     return fd;
 }
+}  // anonymous namespace
 
-void set_device_specific_feature(feature_t feature, int state) {
-    switch (feature) {
-        case POWER_FEATURE_DOUBLE_TAP_TO_WAKE: {
+namespace aidl {
+namespace android {
+namespace hardware {
+namespace power {
+namespace impl {
+
+static constexpr int kInputEventWakeupModeOff = 4;
+static constexpr int kInputEventWakeupModeOn = 5;
+
+using ::aidl::android::hardware::power::Mode;
+
+bool isDeviceSpecificModeSupported(Mode type, bool* _aidl_return) {
+    switch (type) {
+        case Mode::DOUBLE_TAP_TO_WAKE:
+            *_aidl_return = true;
+            return true;
+        default:
+            return false;
+    }
+}
+
+bool setDeviceSpecificMode(Mode type, bool enabled) {
+    switch (type) {
+        case Mode::DOUBLE_TAP_TO_WAKE: {
             int fd = open_ts_input();
             if (fd == -1) {
-                ALOGW("DT2W won't work because no supported touchscreen input devices were found");
-                return;
+                LOG(WARNING)
+                    << "DT2W won't work because no supported touchscreen input devices were found";
+                return false;
             }
             struct input_event ev;
             ev.type = EV_SYN;
             ev.code = SYN_CONFIG;
-            ev.value = state ? INPUT_EVENT_WAKUP_MODE_ON : INPUT_EVENT_WAKUP_MODE_OFF;
+            ev.value = enabled ? kInputEventWakeupModeOn : kInputEventWakeupModeOff;
             write(fd, &ev, sizeof(ev));
             close(fd);
+            return true;
         }
+        default:
+            return false;
     }
 }
+
+}  // namespace impl
+}  // namespace power
+}  // namespace hardware
+}  // namespace android
+}  // namespace aidl