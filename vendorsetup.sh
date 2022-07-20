61f1e8fc6a77de5ae49fb23ab68c0560798f9bc8
Niranjan BR <niranjankannan2003@gmail.com>
2022-07-20 08:58:19 +0000

    violet: Add vendorsetup.sh
    Signed-off-by: Niranjan BR <niranjankannan2003@gmail.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,25 @@
+# Nuke 
+rm -rf hardware/qcom-caf/sm8150/display 
+rm -rf hardware/qcom-caf/sm8150/audio
+rm -rf hardware/qcom-caf/sm8150/media
+rm -rf packages/resources/devicesettings
+
+# Hals
+git clone https://github.com/ArrowOS/android_hardware_qcom_audio -b arrow-12.0-caf-sm8150 hardware/qcom-caf/sm8150/audio 
+git clone https://github.com/ArrowOS/android_hardware_qcom_media -b arrow-12.0-caf-sm8150 hardware/qcom-caf/sm8150/media 
+git clone https://github.com/ArrowOS/android_hardware_qcom_display -b arrow-12.0-caf-sm8150 hardware/qcom-caf/sm8150/display
+
+# Vendor
+git clone https://github.com/VoltageOS-Devices/vendor_xiaomi_violet -b 12 vendor/xiaomi/violet
+
+# Kernel
+git clone https://github.com/mrfox2003/android_kernel_xiaomi_violet.git kernel/xiaomi/violet
+
+# ANX
+git clone -b twelve https://gitlab.com/madhavbiju/vendor_aeonax_anxcamera.git vendor/aeonax/ANXCamera
+
+# Device Settings
+git clone https://github.com/LineageOS/android_packages_resources_devicesettings -b lineage-18.1 packages/resources/devicesettings
+
+# Antradio-library
+git clone https://github.com/LineageOS/android_external_ant-wireless_antradio-library.git -b lineage-19.0 external/ant-wireless/antradio-library
